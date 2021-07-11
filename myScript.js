cc120b0d99a735270fc0f34a4cfb39e02fe09e6d
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2021-07-11 15:31:35 +0530

    Added responsive Nav bar

--- myScript.js
+++ myScript.js
@@ -17,3 +17,13 @@ function mainTabs(a) {
             return false;
 	}
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
