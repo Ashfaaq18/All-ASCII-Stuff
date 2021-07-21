1deb7be8c0bd34a27b73ddfe381bf964c5d5837a
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2021-07-21 20:12:43 +0530

    Added ascii output size option and validation for the input field

--- myScript.js
+++ myScript.js
@@ -78,3 +78,47 @@ function tableFromJson(JSONdata, elementID) {
 	divShowData.appendChild(table);
 	
  }
+ 
+ //---------------- ASCII art stuff ----------------//
+ 
+var asciiRatio = 0.5;
+
+function asciiSizeSet(evt) {
+
+	var e = evt || window.event;
+	var key = e.keyCode || e.which;
+
+	if (!e.shiftKey && !e.altKey && !e.ctrlKey &&
+	// numbers   
+	key >= 48 && key <= 57 ||
+	// Numeric keypad
+	key >= 96 && key <= 105 ||
+	// Backspace
+	key == 8 ||
+	// Home and End
+	key == 35 || key == 36 ||
+	// left and right arrows
+	key == 37 || key == 39 ||
+	// Del and Ins
+	key == 46 || key == 45) {
+		// input is VALID
+		if (document.getElementById("asciiSize").value > 0 && document.getElementById("asciiSize").value < 101) {
+			document.getElementById("inputTest").innerText = document.getElementById("asciiSize").value;
+			asciiRatio = document.getElementById("asciiSize").value/100;
+		} 
+		else if (document.getElementById("asciiSize").value < 1 || document.getElementById("asciiSize").value > 100) {
+			document.getElementById("inputTest").innerText = "out of range";
+		}
+		else {
+			document.getElementById("inputTest").innerText = "invalid input";
+		}
+	}
+	else {
+		// input is INVALID
+		e.returnValue = false;
+		if (e.preventDefault) e.preventDefault();
+	}
+	
+}
+ 
+ 
